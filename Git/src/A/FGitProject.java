b55a0576e35f1c5c40c38c38e038f85ca932c43a
Leynier770 <126425614+LeynierTest@users.noreply.github.com>
2023-02-28 20:39:06 -0500

    Add files via upload

---
+++ Git/src/A/FGitProject.java
@@ -0,0 +1,13 @@
+package A;
+
+public class FGitProject {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		System.out.println("Hola");
+		System.out.println("Hola");
+		System.out.println("Hola");
+		System.out.println("Hola");
+	}
+
+}
---
+++ Git/src/A/SGitClass.java
@@ -0,0 +1,7 @@
+package A;
+
+public class SGitClass {
+	public void say(){
+		System.out.println("Segunda clase");
+	}
+}
